f39c58628fbd8fefb0ae88f4c82a8dcbb659988b
AbhivanshBhatnagar <bhatnagar1102abhivansh@gmail.com>
2024-03-11 22:10:15 +0530

    project init

---
+++ backend/unimanagement/src/main/java/com/project/unimanagement/UnimanagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.unimanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimanagementApplication.class, args);
+	}
+
+}
